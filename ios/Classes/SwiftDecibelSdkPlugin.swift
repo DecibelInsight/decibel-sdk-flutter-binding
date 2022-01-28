f2ae73ca81bcab2ba37bacce54793897b51b6dde
cmartin <cmartin@decibelinsight.com>
2022-01-28 11:11:56 +0100

    DCBLMOB-1073: Added git tracking of XCFramework files. Added files to ignore on .gitignore and updated pigeon methods contract for iOS native

--- ios/Classes/SwiftDecibelSdkPlugin.swift
+++ ios/Classes/SwiftDecibelSdkPlugin.swift
@@ -1,101 +1,108 @@
 import Flutter
 import UIKit
-import DecibelCore
+import DecibelCoreFlutterSigma
 
 public class SwiftDecibelSdkPlugin: NSObject, FlutterPlugin, FLTDecibelSdkApi {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let messenger : FlutterBinaryMessenger = registrar.messenger()
-        let api : FLTDecibelSdkApi = SwiftDecibelSdkPlugin.init()
+        let api : FLTDecibelSdkApi & NSObjectProtocol = SwiftDecibelSdkPlugin.init()
         FLTDecibelSdkApiSetup(messenger, api);
       }
 
-    public func initialize(_ input: FLTSessionMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        if let account = input.account, let property = input.property, let consents = input.consents as? [Int] {
-            DecibelSDK.shared.initialize(account: account, property: property, consents: consents)
-        } else if let account = input.account, let property = input.property {
-            DecibelSDK.shared.initialize(account: account, property: property)
+    public func initializeMsg(_ msg: FLTSessionMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        if let account = msg.account, let property = msg.property, let consents = msg.consents as? [Int] {
+            DecibelSDK.flutter.initialize(account: String(describing: account), property: String(describing: property), consents: consents)
+        } else if let account = msg.account, let property = msg.property {
+            DecibelSDK.flutter.initialize(account: String(describing: account), property: String(describing: property))
         }
-        DecibelSDK.shared.setLogLevel(.info)
+        DecibelSDK.flutter.setLogLevel(.info)
     }
 
-    public func setScreen(_ input: FLTScreenMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        if let screenName = input.screenName {
-            DecibelSDK.shared.set(screen: screenName)
-        }
-    }
-
-    public func setEnableConsents(_ input: FLTConsentsMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        if let consents = input.consents as? [Int] {
-            DecibelSDK.shared.setEnableConsents(consents)
-        }
-    }
-
-    public func setDisableConsents(_ input: FLTConsentsMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        if let consents = input.consents as? [Int] {
-            DecibelSDK.shared.setDisableConsents(consents)
-        }
-    }
-
-    public func sendScreenshot(_ input: FLTScreenshotMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        if let screenshotData = input.screenshotData {
-            DecibelSDK.shared.addImageData(screenshotData.data)
+    public func startScreenMsg(_ msg: FLTStartScreenMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        if let screenName = msg.screenName, let screenId = msg.screenId as? Int{
+            DecibelSDK.flutter.set(screen: screenName, id: screenId)
         }
     }
     
-    public func sendDimension(withString input: FLTDimensionStringMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        guard let dimensionName = input.dimensionName else {
-            return
+    public func endScreenMsg(_ msg: FLTEndScreenMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+//        if let screenName = msg.screenName, let screenId = msg.screenId as? Int{
+//            DecibelSDK.flutter.set(screen: screenName, id: screenId)
+//        }
+    }
+
+    public func setEnableConsentsMsg(_ msg: FLTConsentsMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        if let consents = msg.consents as? [Int] {
+            DecibelSDK.flutter.setEnableConsents(consents)
         }
-        
-        guard let dimensionValue = input.value else {
-            return
+    }
+
+    public func setDisableConsentsMsg(_ msg: FLTConsentsMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        if let consents = msg.consents as? [Int] {
+            DecibelSDK.flutter.setDisableConsents(consents)
         }
-        
-        DecibelSDK.shared.send(dimension: dimensionName, withString: dimensionValue)
+    }
+
+    public func saveScreenshotMsg(_ msg: FLTScreenshotMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        if let screenshotData = msg.screenshotData, let screenId = msg.screenId as? Int, let screenName = msg.screenName,
+           let startFocusTime = msg.startFocusTime as? TimeInterval {
+            DecibelSDK.flutter.saveScreenShot(screenshot: screenshotData.data, id: screenId, screenName: screenName, startFocusTime: startFocusTime)
+        }
     }
     
-    public func sendDimension(withNumber input: FLTDimensionNumberMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        guard let dimensionName = input.dimensionName else {
+    public func sendDimension(withStringMsg msg: FLTDimensionStringMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        guard let dimensionName = msg.dimensionName else {
             return
         }
         
-        guard let dimensionValue = input.value as? Double else {
+        guard let dimensionValue = msg.value else {
             return
         }
         
-        DecibelSDK.shared.send(dimension: dimensionName, withNumber: dimensionValue)
+        DecibelSDK.flutter.send(dimension: dimensionName, withString: dimensionValue)
     }
     
-    public func sendDimension(withBool input: FLTDimensionBoolMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        guard let dimensionName = input.dimensionName else {
+    public func sendDimension(withNumberMsg msg: FLTDimensionNumberMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        guard let dimensionName = msg.dimensionName else {
             return
         }
         
-        guard let dimensionValue = input.value as? Bool else {
+        guard let dimensionValue = msg.value as? Double else {
             return
         }
         
-        DecibelSDK.shared.send(dimension: dimensionName, withBool: dimensionValue)
+        DecibelSDK.flutter.send(dimension: dimensionName, withNumber: dimensionValue)
     }
     
-    public func sendGoal(_ input: FLTGoalMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
-        guard let goalName = input.goal else {
+    public func sendDimension(withBoolMsg msg: FLTDimensionBoolMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+        guard let dimensionName = msg.dimensionName else {
             return
         }
         
-        guard let goalValue = input.value as? Float,
-              let goalCurrency = input.currency as? Int,
-              let currency = DecibelCurrency(rawValue: goalCurrency) else {
-            
-            guard let goalValue = input.value as? Float else {
-                DecibelSDK.shared.send(goal: goalName)
-                return
-            }
-            
-            DecibelSDK.shared.send(goal: goalName, with: goalValue)
+        guard let dimensionValue = msg.value as? Bool else {
             return
         }
         
-        DecibelSDK.shared.send(goal: goalName, with: goalValue, currency: currency)
+        DecibelSDK.flutter.send(dimension: dimensionName, withBool: dimensionValue)
+    }
+    
+    public func sendGoalMsg(_ msg: FLTGoalMessage, error: AutoreleasingUnsafeMutablePointer<FlutterError?>) {
+//        guard let goalName = msg.goal else {
+//            return
+//        }
+//
+//        guard let goalValue = msg.value as? Float,
+//              let goalCurrency = msg.currency as? D,
+//              let currency = DecibelCurrency(rawValue: goalCurrency) else {
+//
+//            guard let goalValue = msg.value as? Float else {
+//                DecibelSDK.flutter.send(goal: goalName)
+//                return
+//            }
+//
+//            DecibelSDK.flutter.send(goal: goalName, with: goalValue)
+//            return
+//        }
+//
+//        DecibelSDK.flutter.send(goal: goalName, with: goalValue, currency: currency)
     }
 }
